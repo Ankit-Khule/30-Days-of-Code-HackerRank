7fc7f2ba952cec446b357526bf916fa593a13855
Ankit Khule <ankit.khule619@gmail.com>
2020-07-24 22:00:39 +0530

    Add files via upload

---
+++ Day-1-Data Types.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 21:56:54 2020
+
+@author: ankit.khule
+"""
+
+# Declare second integer, double, and String variables.
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+j=int(input())
+d1=float(input())
+isc=input()
+S='HackerRank '+ isc
+# Read and save an integer, double, and String to your variables.
+
+
+# Print the sum of both integer variables on a new line.
+print(i+j)
+
+# Print the sum of the double variables on a new line.
+print(d+d1)
+
+# Concatenate and print the String variables on a new line
+print(S)
+# The 's' variable above should be printed first.
