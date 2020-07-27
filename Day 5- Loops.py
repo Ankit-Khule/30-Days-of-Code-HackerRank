c1432e4ee64b49fb72a0f3970e671d41f46eed13
Ankit Khule <ankit.khule619@gmail.com>
2020-07-28 00:07:42 +0530

    Add files via upload

---
+++ Day 5- Loops.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 22:15:29 2020
+
+@author: ankit.khule
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        m=n*i
+        print(n,'x',i,'=', m)
